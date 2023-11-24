eda57ffe4a92192fcde5ec9e31a0e83d0faea241
Rakesh Kumar Regar <rakesh.regar@innoraft.com>
2023-11-24 17:59:02 +0530

    update for capturing ID in controller

--- routing_system_dt231/src/Controller/RoutingSystemDt231Controller.php
+++ routing_system_dt231/src/Controller/RoutingSystemDt231Controller.php
@@ -3,6 +3,7 @@
 namespace Drupal\routing_system_dt231\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Returns responses for Routing system dt231 routes.
@@ -12,11 +13,11 @@ final class RoutingSystemDt231Controller extends ControllerBase {
   /**
    * Builds the response.
    */
-  public function __invoke(): array {
-
+  public function __invoke(Request $request): array {
+    $id = $request->get('id');
     $build['content'] = [
       '#type' => 'item',
-      '#markup' => $this->t('It works!'),
+      '#markup' => $this->t('It works! and ID is = ' . $id),
     ];
 
     return $build;
